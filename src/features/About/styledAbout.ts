3d77b399428aa0d0fcea81aa59bfff4c217dc312
Marcin Izdebski <mizdebski123@gmail.com>
2023-08-30 21:31:48 +0200

    Media query to about section added

--- src/features/About/styledAbout.ts
+++ src/features/About/styledAbout.ts
@@ -20,6 +20,11 @@ export const Header = styled.header`
     align-items: center;
     box-shadow: 11px 13px 28px 0px rgba(0, 0, 0, 0.50);
     animation: ${opacitySlide} 1s;
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.mobileMax}px){
+        height: 120px;
+    };
+
 `;
 
 export const HeaderTitle = styled.h1`
@@ -27,6 +32,11 @@ export const HeaderTitle = styled.h1`
     font-size: 100px;
     color: white;
     animation: ${slideBottom} 1s;
+    
+    @media (max-width: ${({ theme }) => theme.breakPoint.mobileMax}px){
+        font-size: 64px;
+    };
+
 `;
 
 export const Content = styled.section`
@@ -35,10 +45,21 @@ export const Content = styled.section`
     padding: 80px;
     gap: 80px;
     align-items: center;
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.secondBreakPoint}px){
+        display: flex;
+        flex-direction: column-reverse;
+        padding: 12px;
+    };
 `;
 
 export const TextWrapper = styled.div`
     padding: 20px;
+    
+    @media (max-width: ${({ theme }) => theme.breakPoint.mobileMax}px){
+        padding: 0px;
+    };
+
 `;
 
 export const Title = styled.h2`
@@ -47,20 +68,34 @@ export const Title = styled.h2`
     color: ${({ theme }) => theme.color.secondColor};
     margin: 10px;
     animation: ${slideRight} 1s;
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.mobileMax}px){
+        font-size: 28px;
+    };
 `;
 
 export const AboutText = styled.article`
     line-height: 1.2; 
     font-size: 24px;
     animation: ${slideBottom} 1s;
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.mobileMax}px){
+        font-size: 20px;
+    };
 `;
 
 export const Image = styled.img`
+    max-width: 500px;
     width: 500px;
     height: 500px;
     border-radius: 50%;
     border: 6px solid #0F7701;
     box-shadow: 20px 20px 0px 0px #DDD;
     animation: ${slideLeft} 1s;
+    margin: 0 auto;
+
+    @media (max-width: ${({ theme }) => theme.breakPoint.mobileMax}px){
+        max-width: 20px;
+    };
 `;
 
